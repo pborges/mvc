d6f507092019f8fbb27625a3e5fa1fba6e0ebba8
Paulo Borges <pborges475@gmail.com>
2016-04-20 18:03:11 -0700

    RenderError now uses a template, and has an option to pass in a pborges/log.Entry

--- rendererror.go
+++ rendererror.go
@@ -1,15 +1,45 @@
 package mvc
 
 import (
+	"encoding/json"
 	"net/http"
 	"github.com/pborges/log"
 )
 
 func RenderError(err error) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.SetCallDepth(1).WithError(err).Error("error route")
+		entry := log.SetCallDepth(1).WithError(err)
+		RenderDetailedError(entry)(w, r)
+		return
+	}
+}
+
+func RenderDetailedError(err *log.Entry) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err.Error("Error executing request")
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+
+		e := struct {
+			Error      string
+			Package    string
+			Filename   string
+			LineNumber int
+			Fields     map[string]string
+		}{
+			Error:err.Msg,
+			Package:err.Package,
+			Filename:err.Filename,
+			LineNumber:err.Line,
+		}
+		e.Fields = make(map[string]string)
+		for i, k := range err.Keys {
+			j, _ := json.MarshalIndent(err.Values[i], "", "  ")
+			e.Fields[k] = string(j)
+		}
+
+		model := NewViewModel()
+		model.Model = e
+		RenderLayout(model, SharedTemplatePrefix + "error.tmpl.html")(w, r)
 		return
 	}
 }
